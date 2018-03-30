6322e8c914a70f43e326b513daa02fe5e6037c4e
Douglas <themw63boss@outlook.com>
2018-03-29 21:35:43 -0300

    remoçao dos .java

--- Escola.java
+++
@@ -1,8 +0,0 @@
-    public class Escola {
-
-        public static void main(String args[]){
-            Aluno alun = new Aluno();
-            Pessoa pes = new Pessoa("Douglas","themw63boss@outlook.com");
-            Professor prof = new Professor();
-        }
-}
